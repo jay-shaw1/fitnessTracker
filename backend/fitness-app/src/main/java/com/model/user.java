f4feddda13cdd5b86f5edd0794bcc7730c5f7158
Jarren <jarrens01@gmail.com>
2026-02-24 23:34:50 -0500

    Connect Spring boot to database in backend.

---
+++ backend/fitness-app/src/main/java/com/model/user.java
@@ -0,0 +1,13 @@
+@Entity
+@Table(name = "users")
+public class User {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    private String username;
+    private String email;
+    private String password;
+
+    //getters and setters
+}
