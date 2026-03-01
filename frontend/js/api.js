4692dce0f6ee8be9bda07222d4c46645ee95abb1
Jarren <jarrens01@gmail.com>
2026-02-28 21:10:37 -0500

    Start building connection from front to backend.

---
+++ frontend/js/api.js
@@ -0,0 +1,19 @@
+const BASE_URL = "https://localhost:8080/api" //Store root to backend
+
+async function createUser(username, email, password){
+    const response = await fetch(`${BASE_URL}/users`,
+        {
+            method : 'POST',
+            headers : {
+                'Content-Type' : 'application/json'
+            },
+            body : JSON.stringify({ username, email, password })
+        }
+    );
+    return response.json();
+}
+
+async function getWorkouts(){
+    const response = await fetch(`${BASE_URL}/workouts`);
+    return response.json();
+}
