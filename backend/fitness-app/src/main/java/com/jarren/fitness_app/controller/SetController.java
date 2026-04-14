3eeae590fa75566fd1271dabccd330440f923ed8
Jarren <jarrens01@gmail.com>
2026-04-13 20:24:19 -0400

    Add endpoint and finder method to Exercise Set.

--- backend/fitness-app/src/main/java/com/jarren/fitness_app/controller/SetController.java
+++ backend/fitness-app/src/main/java/com/jarren/fitness_app/controller/SetController.java
@@ -9,6 +9,7 @@ import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import java.util.List;
 
 @CrossOrigin(origins = "*")
@@ -28,4 +29,10 @@ public class SetController {
     public List<ExerciseSet> getAllSets() {
         return setRepo.findAll();
     }
+
+    @GetMapping("/exercise/{exerciseId}")
+    public List<ExerciseSet> getSetsByExercise(@PathVariable Long exerciseId) {
+        return setRepo.findByExerciseId(exerciseId);
+    }
+    
 }
--- backend/fitness-app/src/main/java/com/jarren/fitness_app/repo/SetRepository.java
+++ backend/fitness-app/src/main/java/com/jarren/fitness_app/repo/SetRepository.java
@@ -2,6 +2,8 @@ package com.jarren.fitness_app.repo;
 
 import org.springframework.data.jpa.repository.JpaRepository;
 import com.jarren.fitness_app.model.ExerciseSet;
+import java.util.List;
 
 public interface SetRepository extends JpaRepository<ExerciseSet, Long>{
+    List<ExerciseSet> findByExerciseId(Long exerciseId);
 }
